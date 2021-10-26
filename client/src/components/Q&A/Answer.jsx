70febde7c9239555b12ae948f013fa87a8f9a068
Chris Turcios <christopher.d.turcios@gmail.com>
2021-10-25 21:36:50 -0700

    Create Question & Answer components into class components (will change to functional components if needed upon further investigation)

---
+++ client/src/components/Q&A/Answer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Answer extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div>Answer</div>
+    )
+  }
+}
+
+export default Answer;
---
+++ client/src/components/Q&A/Question.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+class Question extends React.Component {
+  constructor(props) {
+    super(props);
+
+  }
+
+  render() {
+    return (
+      <div>Question</div>
+    )
+  }
+}
+
+export default Question;
