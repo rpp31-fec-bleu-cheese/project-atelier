6e91bb2b539a5ee6ed105e23dfc7c5be0efb750a
Chris Turcios <christopher.d.turcios@gmail.com>
2021-10-26 16:24:08 -0700

    import mock data into main Q&A component to render

--- client/src/components/Q&A/Q&A.jsx
+++ client/src/components/Q&A/Q&A.jsx
@@ -2,13 +2,14 @@ import React from 'React';
 
 import Search from './Search.jsx';
 import Question from './Question.jsx';
+import mockData from './mockData.js';
 
 class QandA extends React.Component {
 
   constructor(props) {
     super(props);
     this.state = {
-
+      questions: mockData.results
     };
   }
 
@@ -16,7 +17,8 @@ class QandA extends React.Component {
     return(
       <div id='QandA'>
         <Search />
-        <Question />
+        {mockData.results
+          .map(question => <Question key={question.question_id} question={question.question_body}/>)}
       </div>
     )
   }
