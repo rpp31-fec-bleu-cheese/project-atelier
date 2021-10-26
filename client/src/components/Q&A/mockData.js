de3fbc871fb473799aa2b52199e82f90e31fd0e8
Chris Turcios <christopher.d.turcios@gmail.com>
2021-10-26 16:23:50 -0700

    Add mock data to use for temporary rendering

---
+++ client/src/components/Q&A/mockData.js
@@ -0,0 +1,85 @@
+const mockData = {
+  "product_id": "59556",
+  "results": [
+      {
+          "question_id": 513738,
+          "question_body": "What fabric is the bottom made of?",
+          "question_date": "2018-09-12T00:00:00.000Z",
+          "asker_name": "iluvcatz",
+          "question_helpfulness": 7,
+          "reported": false,
+          "answers": {
+              "4811943": {
+                  "id": 4811943,
+                  "body": "The rubber on the bottom wears thin quickly",
+                  "date": "2018-10-12T00:00:00.000Z",
+                  "answerer_name": "iluvbirds",
+                  "helpfulness": 4,
+                  "photos": []
+              },
+              "4811944": {
+                  "id": 4811944,
+                  "body": "Rubber",
+                  "date": "2018-10-12T00:00:00.000Z",
+                  "answerer_name": "iluvbirds",
+                  "helpfulness": 1,
+                  "photos": []
+              },
+              "4811992": {
+                  "id": 4811992,
+                  "body": "Its a rubber sole",
+                  "date": "2018-10-12T00:00:00.000Z",
+                  "answerer_name": "iluvdogz",
+                  "helpfulness": 6,
+                  "photos": []
+              },
+              "4811993": {
+                  "id": 4811993,
+                  "body": "Some kind of recycled rubber, works great!",
+                  "date": "2018-10-12T00:00:00.000Z",
+                  "answerer_name": "iluvdogz",
+                  "helpfulness": 7,
+                  "photos": []
+              }
+          }
+      },
+      {
+          "question_id": 513739,
+          "question_body": "Why is this product cheaper here than other sites?",
+          "question_date": "2017-08-12T00:00:00.000Z",
+          "asker_name": "toofast",
+          "question_helpfulness": 4,
+          "reported": false,
+          "answers": {
+              "4811945": {
+                  "id": 4811945,
+                  "body": "This product is overstocked here!",
+                  "date": "2017-09-12T00:00:00.000Z",
+                  "answerer_name": "toofast",
+                  "helpfulness": 2,
+                  "photos": []
+              }
+          }
+      },
+      {
+          "question_id": 513737,
+          "question_body": "Where is this product made?",
+          "question_date": "2017-02-06T00:00:00.000Z",
+          "asker_name": "cleopatra",
+          "question_helpfulness": 2,
+          "reported": false,
+          "answers": {
+              "4812011": {
+                  "id": 4812011,
+                  "body": "China",
+                  "date": "2017-03-06T00:00:00.000Z",
+                  "answerer_name": "ceasar",
+                  "helpfulness": 0,
+                  "photos": []
+              }
+          }
+      }
+  ]
+}
+
+export default mockData;
