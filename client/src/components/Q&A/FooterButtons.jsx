dfb38967e303faec350ec86633488f05fa0be553
Chris Turcios <christopher.d.turcios@gmail.com>
2021-10-26 16:49:33 -0700

    add new jsx file for the section footer buttons

---
+++ client/src/components/Q&A/FooterButtons.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const FooterButtons = () => {
+  return (
+    <div className="footer-buttons">
+      <button>MORE ANSWERED QUESTIONS</button>
+      <button>ADD A QUESTION +</button>
+    </div>
+  )
+};
+
+export default FooterButtons;
