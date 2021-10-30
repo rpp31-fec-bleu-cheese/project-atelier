a630a93f37f421813f1062f57a74b5d80110661e
Chris Turcios <christopher.d.turcios@gmail.com>
2021-10-29 19:55:48 -0700

    Add styling to footer buttons

--- client/src/components/Q&A/FooterButtons.jsx
+++ client/src/components/Q&A/FooterButtons.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 const FooterButtons = () => {
   return (
     <div className="footer-buttons">
-      <button>MORE ANSWERED QUESTIONS</button>
-      <button>ADD A QUESTION +</button>
+      <button className="additional-answers-button">MORE ANSWERED QUESTIONS</button>
+      <button className="add-question-button">ADD A QUESTION +</button>
     </div>
   )
 };
