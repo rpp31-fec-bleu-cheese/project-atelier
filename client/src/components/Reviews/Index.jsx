15ad3dcf12d9b311bf5fb8534d4681f86862fabc
joshdfuqua <joshdfuqua@gmail.com>
2021-11-03 22:03:51 -0400

    Refactored the main ratings/reviews component to make API calls. Set up the ratings/reviews container to only render "Loading..." until the API calls have completed.

--- client/src/components/Reviews/Index.jsx
+++ client/src/components/Reviews/Index.jsx
@@ -12,106 +12,73 @@ class Reviews extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      currentRatings: {
-        "1": "",
-        "2": "1",
-        "3": "1",
-        "4": "2",
-        "5": "1"
-      },
-      currentReviews: [
-        {
-            "review_id": 1016929,
-            "rating": 3,
-            "summary": "I'm enjoying wearing these shades",
-            "recommend": true,
-            "response": "",
-            "body": "Comfortable and practical.",
-            "date": "2019-04-14T00:00:00.000Z",
-            "reviewer_name": "shortandsweeet",
-            "helpfulness": 5,
-            "photos": [
-                {
-                    "id": 1945562,
-                    "url": "https://images.unsplash.com/photo-1560570803-7474c0f9af99?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=975&q=80"
-                },
-                {
-                    "id": 1945563,
-                    "url": "https://images.unsplash.com/photo-1561693532-9ff59442a7db?ixlib=rb-1.2.1&auto=format&fit=crop&w=975&q=80"
-                },
-                {
-                    "id": 1945564,
-                    "url": "https://images.unsplash.com/photo-1487349384428-12b47aca925e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1650&q=80"
-                }
-            ]
-        },
-        {
-            "review_id": 1016927,
-            "rating": 4,
-            "summary": "I am liking these glasses",
-            "recommend": true,
-            "response": "Glad you're enjoying the product!",
-            "body": "They are very dark. But that's good because I'm in very sunny spots",
-            "date": "2019-06-23T00:00:00.000Z",
-            "reviewer_name": "bigbrotherbenjamin",
-            "helpfulness": 5,
-            "photos": []
-        },
-        {
-            "review_id": 1016928,
-            "rating": 4,
-            "summary": "They look good on me",
-            "recommend": true,
-            "response": "",
-            "body": "I so stylish and just my aesthetic.",
-            "date": "2019-03-12T00:00:00.000Z",
-            "reviewer_name": "fashionperson",
-            "helpfulness": 1,
-            "photos": []
-        },
-        {
-            "review_id": 1016931,
-            "rating": 2,
-            "summary": "This product was ok!",
-            "recommend": false,
-            "response": "",
-            "body": "They're fine but I wouldn't buy again.",
-            "date": "2019-05-23T00:00:00.000Z",
-            "reviewer_name": "anyone",
-            "helpfulness": 0,
-            "photos": []
-        },
-        {
-            "review_id": 1016930,
-            "rating": 5,
-            "summary": "I'm not a fan!",
-            "recommend": false,
-            "response": "Sorry to hear. Is there anything in particular you don't like?",
-            "body": "I don't like them",
-            "date": "2019-06-16T00:00:00.000Z",
-            "reviewer_name": "negativity",
-            "helpfulness": 0,
-            "photos": []
-        }
-    ],
-    characteristics: {
-      "Quality": {
-          "id": 199849,
-          "value": "4.2000000000000000"
-      }
-  }
+      currentRatings: {},
+      currentReviews: [],
+      characteristics: {}
     };
   }
 
+  componentDidMount() {
+    $.ajax({
+      url: 'http://localhost:3000/reviews',
+      method: 'GET',
+      data: {
+        product_id: this.props.product_id
+      },
+      success: reviews => {
+        $.ajax({
+          url: 'http://localhost:3000/reviews/meta',
+          method: 'GET',
+          data: {
+            product_id: this.props.product_id
+          },
+          success: metaData => {
+            let ratings = {
+              1: 0,
+              2: 0,
+              3: 0,
+              4: 0,
+              5: 0
+            };
+            let characteristics = {}
+            for (let rate in metaData.ratings) {
+              ratings[rate] = metaData.ratings[rate]
+            }
+            for (let char in metaData.characteristics) {
+              characteristics[char] = metaData.characteristics[char]
+            }
+            this.setState({
+              currentRatings: ratings,
+              currentReviews: reviews.results,
+              characteristics: characteristics
+            })
+          },
+          error: (_, __, errString) => console.log(errString)
+        })
+      },
+      error: (_, __, errString) => console.log(errString)
+    })
+  }
+
   render() {
-    return(
-      <div id='RatingsReviews'>
-        <Header />
-        <RatingsContainer ratings={this.state.currentRatings} reviews={this.state.currentReviews}/>
-        <BreakdownContainer characteristics={this.state.characteristics}/>
-        <ReviewsContainer />
-      </div>
-    )
+    if (this.state.currentReviews.length > 0) {
+      return (
+        <div id='RatingsReviews'>
+          <Header />
+          <RatingsContainer ratings={this.state.currentRatings} reviews={this.state.currentReviews}/>
+          <BreakdownContainer characteristics={this.state.characteristics}/>
+          <ReviewsContainer />
+        </div>
+      )
+    } else {
+      return (
+        <div id='RatingsReviews'>
+          <Header />
+          Loading...
+        </div>
+      )
+    }
+
   }
 };
 
--- config.js
+++ config.js
@@ -1,1 +1,3 @@
-module.exports = {};
+module.exports = {
+  API_KEY: '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'
+};
