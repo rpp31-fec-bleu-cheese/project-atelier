7d5ebb8b0378ed284d038a400f7c0d48c4362229
Sangeetha Nair <sangeetha.prasanth@gmail.com>
2021-11-05 13:09:45 -0700

    modified APP.jsx to include the function to handle outfit change

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -6,20 +6,24 @@ import ProductInfo from './ProductInfo/ProductInfo.jsx';
 import Related_Outfit from './Related_Outfit/Related_Outfit.jsx';
 import QandA from './Q&A/Q&A.jsx';
 import Reviews from './Reviews/Reviews.jsx';
+import data from '../../data/Related_Outfit.js';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      productId: 59558
+      productId: 59558,
+      outfitIds: data.outfitIds
     };
     this.relatedOutfitProductClick = this.relatedOutfitProductClick.bind(this);
+    this.changeInOutfit = this.changeInOutfit.bind(this);
 
   }
   relatedOutfitProductClick(event, productId){
-    console.log('event.target.id:',event.target.id)
+
   if(event.target.id !== 'Overlay_Star' && event.target.id !== 'Overlay_Circle') {
     console.log('inside click function productId:', productId);
+
     //update the productId
 
     this.setState({
@@ -28,13 +32,29 @@ class App extends React.Component {
    }
 
   }
+  changeInOutfit(event, productId, todo) {
+    console.log('inside changeInOutfit in APP component')
+    console.log('productId',productId);
+    var outfitIds = this.state.outfitIds.slice(0)
+    if(todo === "Add") {
+      outfitIds.push(productId);
+    } else if(todo === "Delete") {
+      console.log("deleting the product from outfits");
+      var index = outfitIds.indexOf(productId);
+      outfitIds.splice(index,1);
+
+    }
+    this.setState({
+      outfitIds: outfitIds
+    })
+  }
   render() {
     return (
       <div id='App'>
         <Header />
         <SiteMessage />
         <ProductInfo />
-        <Related_Outfit productId={this.state.productId} productClick={this.relatedOutfitProductClick}/>
+        <Related_Outfit productId={this.state.productId} changeInOutfit={this.changeInOutfit} outfitIds={this.state.outfitIds} productClick={this.relatedOutfitProductClick}/>
         <QandA />
         <Reviews />
       </div>
