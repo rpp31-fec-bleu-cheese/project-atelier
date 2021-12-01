f1547961e0f2523bde2e419f6d71fff4f019f5ca
Cameron Colaco <wearenotgood@gmail.com>
2021-11-30 20:57:52 -0800

    Added API_KEY for development mode in config.js; ran git rm --cached config.js so config won't commit and will stay in gitignore.

--- client/src/index.js
+++ client/src/index.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App.jsx';
-import cam_token from '../../config.js';
 
 ReactDOM.render(<App />, document.getElementById('appContainer'));
